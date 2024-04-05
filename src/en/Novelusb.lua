a9d4b7f480fcf3cc7eeca43a68a7375a16bb12bd
Fasih Ur Rehman <s2023266121@umt.edu.pk>
2024-04-05 22:01:54 +0500

    No Cover Issue (#522) Fixed

--- src/en/Novelusb.lua
+++ src/en/Novelusb.lua
@@ -1,4 +1,4 @@
--- {"id":95564,"ver":"1.0.1,"libVer":"1.0.0","author":"Confident-hate"}
+-- {"id":95564,"ver":"1.0.1","libVer":"1.0.0","author":"Confident-hate"}
 
 local baseURL = "https://novelusb.com"
 
